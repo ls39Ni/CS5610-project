80938a872b825b826e87f6642bddd03d6f34865d
cliffdawg <cliffordyin@gmail.com>
2024-05-09 22:32:00 -0700

    Added name and section

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,6 +6,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div id="wd-labs">
+      <h1>Clifford Yin, Section 01</h1><br/>
       <h1>Labs</h1>
       <TOC />
       <Routes>
